49b8be04eef8a98def690ebe93ae94f1c1da4aaa
erramuzpe <asier.erramuzpe@gmail.com>
2017-05-15 11:19:54 +0200

    more tests

--- src/matrix_detection.py
+++ src/matrix_detection.py
@@ -3,8 +3,8 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('../data/raw/1.jpg')
-gray = cv2.imread('../data/raw/1.jpg',0)
+img = cv2.imread('./data/raw/1.jpg')
+gray = cv2.imread('./data/raw/1.jpg',0)
 
 ret,thresh = cv2.threshold(gray,127,255,1)
 
@@ -37,3 +37,40 @@ cv2.destroyAllWindows()
 http://opencvpython.blogspot.com.es/2012/06/sudoku-solver-part-2.html
 http://stackoverflow.com/questions/11424002/how-to-detect-simple-geometric-shapes-using-opencv
 https://dsp.stackexchange.com/questions/3595/finding-squares-in-image
+
+
+
+import cv2
+import numpy as np
+
+img = cv2.imread('./data/raw/1.jpg')
+gray = cv2.imread('./data/raw/1.jpg',cv2.COLOR_BGR2GRAY)
+
+gray = cv2.GaussianBlur(gray,(5,5),0)
+thresh = cv2.adaptiveThreshold(gray,255,1,1,11,2)
+
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+
+biggest = None
+max_area = 0
+for i in contours:
+        area = cv2.contourArea(i)
+        if area > 100:
+                peri = cv2.arcLength(i,True)
+                approx = cv2.approxPolyDP(i,0.02*peri,True)
+                if area > max_area and len(approx)==4:
+                        biggest = approx
+                        max_area = area
+                        
+cv2.drawContours()       
+
+
+
+
+cv2.imwrite('test.png',gray)
+
+
+
+
+                 
